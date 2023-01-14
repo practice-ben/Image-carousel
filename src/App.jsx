e2007530ad9bd39bcf38a006db5a94032c1fd2b4
Benson Njuguna <njugunab655@gmail.com>
2023-01-14 08:44:34 +0300

    Added an images array

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,16 @@ export default function App() {
   const [image, setImage] = React.useState("https://images.pexels.com/photos/2387418/pexels-photo-2387418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1")
 
   const imageArr = [
-
+    "https://images.pexels.com/photos/2387418/pexels-photo-2387418.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/1287145/pexels-photo-1287145.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/2662116/pexels-photo-2662116.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/13790095/pexels-photo-13790095.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "https://images.pexels.com/photos/3030833/pexels-photo-3030833.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+    "https://images.pexels.com/photos/147411/italy-mountains-dawn-daybreak-147411.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/442555/pexels-photo-442555.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/5095808/pexels-photo-5095808.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/3017093/pexels-photo-3017093.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+    "https://images.pexels.com/photos/281000/pexels-photo-281000.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
   ]
 
   return(
